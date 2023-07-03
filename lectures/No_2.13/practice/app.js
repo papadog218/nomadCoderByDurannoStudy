e98581d57cc6fd639395ce1c1824f971d498735e
papadog218 <mki218@naver.com>
2023-07-03 22:27:25 +0900

    폴더명 수정

---
+++ lectures/No_2.13/practice/app.js
@@ -0,0 +1,6 @@
+// const age = prompt("How old are you??");
+// console.log(age, parseInt(age));
+
+// 함수는 내부에서 외부 순서대로 동작한다 (= 입력을 받은 후 형변환을 함)
+const age = parseInt(prompt("How old are you??"));
+console.log(age);
---
+++ lectures/No_2.14/practice/app.js
@@ -0,0 +1,3 @@
+// if 조건문으로 NaN 구분하기
+const age = parseInt(prompt("How old are you??"));
+console.log(age);
