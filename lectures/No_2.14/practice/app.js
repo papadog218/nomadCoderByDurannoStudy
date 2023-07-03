879ab87e39fed425ae648816f83b6654d24bf1b1
papadog218 <mki218@naver.com>
2023-07-03 22:39:12 +0900

    #2.14 강의 정리

--- lectures/No_2.14/practice/app.js
+++ lectures/No_2.14/practice/app.js
@@ -1,3 +1,12 @@
-// if 조건문으로 NaN 구분하기
+// isNaN(): 내장함수이고 넘긴 값이 숫자면 false 아니면 true를 반환함
 const age = parseInt(prompt("How old are you??"));
-console.log(age);
+
+// console.log(isNaN(age));
+
+
+// if 조건문으로 age가 숫자인지 아닌지 구별하기
+if (isNaN(age)) {
+    console.log("Please write a number");
+} else {
+    console.log("Thank you!");
+}
