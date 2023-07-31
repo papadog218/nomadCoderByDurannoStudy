8dfdc5a7a63e2f6236f81e87b8f8ed8fefd0d482
용환 김 <yhKim@duranno.com>
2023-07-31 19:53:25 +0900

    시계 한자리 숫자 버그 수정

---
+++ lectures/clock/practice/No_5.2/js/clock.js
@@ -0,0 +1,19 @@
+const clock = document.querySelector('#clock');
+
+// clock.innerHTML = "laalalal";
+
+// setInterval() 설명 후 끝
+function getTime() {
+    const nowDate = new Date();
+    const hh = String(nowDate.getHours()).padStart(2, '0');
+    const mm = String(nowDate.getMinutes()).padStart(2, '0');
+    const ss = String(nowDate.getSeconds()).padStart(2, '0');
+    clock.innerText = `${hh}:${mm}:${ss}`;
+};
+
+getTime(); // 먼저 보여주고 1초 지날 때마다 보이게
+setInterval(getTime, 1000); // 얘만하면 시간을 1초 기다렸다가 보여줌
+
+// setInterval()은 시간 마다 실행하고 setTimeout()은 시간 후에 실행
+// setTimeout(sayHello, 5000);
+
